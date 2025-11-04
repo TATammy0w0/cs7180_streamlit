d36b932f273a452b8755ae2c43254bcce21e009a
Tammy Ma <ma.wenjing1@northeastern.edu>
2025-11-04 01:57:38 -0800

    set up card ui template

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import streamlit_shadcn_ui as ui
 
-st.title("🎈 My new app")
+st.title("🩺 Multi-Disease Risk Prediction")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Help you understand your health risks from your health report, head over to [placeholder link](https://docs.streamlit.io/)."
 )
 
 # shadow ui component example
 with ui.card(key="card1"):
-    ui.element("span", children=["Email"], className="text-gray-400 text-sm font-medium m-1", key="label1")
-    ui.element("input", key="email_input", placeholder="Your email")
+    ui.element("span", children=["Your Height (cm)"], className="text-gray-400 text-sm font-medium m-1", key="label1")
+    ui.element("input", key="user_height", placeholder="e.g. 170")
 
-    ui.element("span", children=["User Name"], className="text-gray-400 text-sm font-medium m-1", key="label2")
-    ui.element("input", key="username_input", placeholder="Create a User Name")
+    ui.element("span", children=["Your Weight (kg)"], className="text-gray-400 text-sm font-medium m-1", key="label2")
+    ui.element("input", key="user_weight", placeholder="e.g. 70.5")
+
+    ui.element("span", children=["Your Age (years)"], className="text-gray-400 text-sm font-medium m-1", key="label3")
+    ui.element("input", key="user_age", placeholder="e.g. 25")
+
     ui.element("button", text="Submit", key="button", className="m-1")
